cfde0ac643b53b7034a975ba9d2a548ec6ecd18e
arpit-artechnolabs <134257230+arpit-artechnolabs@users.noreply.github.com>
2023-05-22 13:12:04 +0530

    Files added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import RegistrationForm from './components/RegistrationForm';
+import './css/App.css';
+
+function App() {
+  return (
+    <>
+     <RegistrationForm/>
+    </>
+  );
+}
+
+export default App;
